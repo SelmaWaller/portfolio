2d0235432e5f55553214a86c61d238831631768c
Selma <selma.waller@glasspaper.no>
2024-04-26 22:52:21 +0200

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,42 @@
+import VitePluginSassDts from "vite-plugin-sass-dts";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+  ],
+  router: {
+    options: {
+      scrollBehaviorType: "smooth",
+    },
+  },
+  app: {
+    head: {
+      title: "Selma Waller Ísfeld",
+      meta: [
+        {
+          name: "description",
+          content: "CV and portfolio for Selma Waller Ísfeld",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  },
+  vite: {
+    plugins: [
+      VitePluginSassDts({
+        /* options */
+      }),
+    ],
+  },
+  css: ["~/src/assets/css/main.scss"],
+});
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+        <h1>this should be styled from the layout.</h1>
+</template>
