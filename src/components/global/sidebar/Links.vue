4465c7327043ba4cd20e4f51e44ac3b75bb972b9
Selma <selma.waller@glasspaper.no>
2024-05-02 23:47:35 +0200

    menu items updates
    - add arrow svg to menu items #3
    - correct styling to active menu links #4

--- src/components/global/sidebar/Links.vue
+++ src/components/global/sidebar/Links.vue
@@ -1,26 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps(["color"]);
+import Arrow from "~/src/components/svg/Arrow.vue";
+</script>
 
 <template>
   <div class="sidebar__menu">
     <ul>
-      <li><NuxtLink class="sidebar__menu--item" to="/cv">CV</NuxtLink></li>
       <li>
-        <NuxtLink class="sidebar__menu--item" to="/about-me">About me</NuxtLink>
+        <span><Arrow color="#000" /></span>
+        <NuxtLink class="sidebar__menu--item" to="/cv"> CV</NuxtLink>
       </li>
       <li>
-        <NuxtLink class="sidebar__menu--item" to="/web-apps">Web apps</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink class="sidebar__menu--item" to="/figma">Figma</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink class="sidebar__menu--item" to="/css-drawings"
-          >CSS drawings</NuxtLink
+        <span><Arrow color="#000" /></span>
+        <NuxtLink class="sidebar__menu--item" to="/about-me">
+          About me</NuxtLink
         >
       </li>
       <li>
-        <NuxtLink class="sidebar__menu--item special" to="/prints"
-          >Prints</NuxtLink
+        <span><Arrow color="#000" /></span>
+        <NuxtLink class="sidebar__menu--item" to="/web-apps">
+          Web apps</NuxtLink
+        >
+      </li>
+      <li>
+        <span><Arrow color="#000" /></span>
+        <NuxtLink class="sidebar__menu--item" to="/figma"> Figma</NuxtLink>
+      </li>
+      <li>
+        <span><Arrow color="#000" /></span>
+        <NuxtLink class="sidebar__menu--item" to="/css-drawings">
+          CSS drawings</NuxtLink
+        >
+      </li>
+      <li>
+        <span><Arrow color="#000" /></span>
+        <NuxtLink class="sidebar__menu--item special" to="/prints">
+          Prints</NuxtLink
         >
       </li>
     </ul>
