4151403ed496033a82f5859a686ea58f57618c08
Selma <selmawaller@gmail.com>
2024-05-13 11:13:34 +0200

    create single card component with gifs on hover

--- pages/figma.vue
+++ pages/figma.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import ProjectCard from "~/src/components/global/ProjectCard.vue";
 import projects from "~/data/figma.json";
+import ProjectCardGrid from "~/src/components/global/ProjectCardGrid.vue";
 </script>
 
 <template>
-  <div><ProjectCard :projects="projects.projects" /></div>
+  <div>
+    <ProjectCardGrid :projects="projects.projects" />
+  </div>
 </template>
